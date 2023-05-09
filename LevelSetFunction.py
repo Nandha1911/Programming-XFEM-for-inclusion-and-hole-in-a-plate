a773707858934f13ae05b0a1b0556f1275ede87d
Nandha Gopal Mariappan <129469684+Nandha1911@users.noreply.github.com>
2023-05-09 04:36:13 +0200

    Update LevelSetFunction.py
    Error rectification

--- LevelSetFunction.py
+++ LevelSetFunction.py
@@ -5,14 +5,15 @@ def LevelSet(temp,a,b,r):
   temp1=[];
   for j in range(len(temp)):
         phi=math.sqrt(((temp[j][0]-a)**2)+((temp[j][1]-b)**2))-r; 
+        doubt=round(phi,3)
         ##Enrichment Function rule:-
         ##If phi value is greater than 0,then the node lies outside the circle and the enrichment 
         ##function will be V(x)=1
         ##If phi value is less than 0,then the node lies inside/on the circle and the enrichment 
         ##function will be V(x)=0
-        if phi > 0: #or #phi ==0:
+        if round(phi,3) > 0: #or #phi ==0:
             phi=1;
-        elif phi ==0:
+        elif math.isclose(round(phi,3),0) == True:
             phi = 0;
         else:
             phi=-1;
@@ -20,4 +21,4 @@ def LevelSet(temp,a,b,r):
                                       #in order to calculate whether the element will be cut by an
                                       #curve or not.....(It proceeds to the if..else..statement)
                                       
-  return temp1                               
+  return temp1                         
