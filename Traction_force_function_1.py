955dfb6e0cb047b0357e08603842d5e103ec8a13
Nandha Gopal Mariappan <129469684+Nandha1911@users.noreply.github.com>
2023-05-17 00:51:39 +0200

    Update Traction_force_function_1.py
    Rectification

--- Traction_force_function_1.py
+++ Traction_force_function_1.py
@@ -2,105 +2,173 @@ import numpy as np  ##Dupicate copy
 
 
 def Tractforce(i,Bmatstd,Bmatenr,element,elementc,elementnoc,tractforces,t,wi,belement,
-              N1,N2,N3,N4,c1,c2,xi1,xi2,phivalues,Edge,cornerelements):
+              N1,N2,N3,N4,c1,c2,phivalues,Edge,l,F2):
+ 
+ for i in range(len(element)):
    sizoff=np.shape(Bmatstd);
    sizoff1=np.shape(Bmatenr);
    #for i in range(len(element)): 
-   fbstd=np.zeros(sizoff[1]);        #This is for elements without forces(standard)
-   fbenr=np.zeros([3,16]);
+   c=int((sizoff[1])/2)
+   d=int((sizoff1[1])/2)
+   ftstd1=np.zeros(c);        #This is for elements without forces(standard)
+   ftenr1=np.zeros(d);
+   ftstd=np.zeros((sizoff[1]));        #This is for elements without forces(standard)
+   ftenr=np.zeros((sizoff1[1]));
+  # xi1 = np.array([-1,1,1,-1]);
+  # xi2 = np.array([-1,-1,1,1]);
    #fbenr=np.zeros(sizoff1[1]);        #This is for elements without forces(Enriched)
        #fbenr=np.zeros(sizoff1[1]); 
-   def shapefunctract(i,elemtyp,j):
-        N11=N1.subs(c1,xi1[j]).subs(c2,xi2[j]);
-        N12=N2.subs(c1,xi1[j]).subs(c2,xi2[j]);
-        N13=N3.subs(c1,xi1[j]).subs(c2,xi2[j]);
-        N14=N4.subs(c1,xi1[j]).subs(c2,xi2[j]);
-        if elemtyp=="Std":
-           Nstd=np.array([[N11,0,N12,0,N13,0,N14,0],[0,N11,0,N12,0,N13,0,N14]]);
-           return Nstd
+   def shapefunctract(i,elemtyp,j,Edge1):
+        if Edge1=='Edge1':
+            xi1 = np.array([1,1]);
+            xi2 = np.array([-1,1]);
+            N12=N2.subs(c1,xi1[j]).subs(c2,xi2[j]);
+            N13=N3.subs(c1,xi1[j]).subs(c2,xi2[j]);
+            if elemtyp=="Std":
+               Nstd=np.array([[N12,0,N13,0],[0,N12,0,N13]]);
+               return Nstd
 
-        elif elemtyp=="Enr" :
-           phi=phivalues[i];
-           Nenr=np.array([[N11,0,(N11*phi[0]),0,N12,0,(N12*phi[1]),0,N13,0,(N13*phi[2]),0,N14,0,(N14*phi[3]),0],\
-                           [0,N11,0,(N11*phi[0]),0,N12,0,(N12*phi[1]),0,N13,0,(N13*phi[2]),0,N14,0,\
-                                          (N14*phi[3])]]);
-           return Nenr             
+            elif elemtyp=="Enr" :
+               phi=phivalues[i];
+               Nenr=np.array([[N12,0,(N12*phi[1]),0,N13,0,(N13*phi[2]),0],\
+                              [0,N12,0,(N12*phi[1]),0,N13,0,(N13*phi[2])]]);
+               return Nenr
+        elif Edge1 == 'Edge2':
+            xi1 = np.array([1,-1]);
+            xi2 = np.array([1,1]);
+            N13=N3.subs(c1,xi1[j]).subs(c2,xi2[j]);
+            N14=N4.subs(c1,xi1[j]).subs(c2,xi2[j]);
+            if elemtyp=="Std":
+               Nstd=np.array([[N13,0,N14,0],[0,N13,0,N14]]);
+               return Nstd
+
+            elif elemtyp=="Enr" :
+               phi=phivalues[i];
+               Nenr=np.array([[N13,0,(N13*phi[2]),0,N14,0,(N14*phi[3]),0],\
+                              [0,N13,0,(N13*phi[2]),0,N14,0,(N14*phi[3])]]);
+               return Nenr
+        elif Edge1 == 'Edge3':
+               xi1 = np.array([-1,-1]);
+               xi2 = np.array([-1,1]);
+               N11=N1.subs(c1,xi1[j]).subs(c2,xi2[j]);
+               N14=N4.subs(c1,xi1[j]).subs(c2,xi2[j]);
+               if elemtyp=="Std":
+                  Nstd=np.array([[N11,0,N14,0],[0,N11,0,N14]]);
+                  return Nstd
+
+               elif elemtyp=="Enr" :
+                  phi=phivalues[i];
+                  Nenr=np.array([[N11,0,(N11*phi[0]),0,N14,0,(N14*phi[3]),0],\
+                                 [0,N11,0,(N11*phi[0]),0,N14,0,(N14*phi[3])]]);
+                  return Nenr
+        elif Edge1 == 'Edge4':
+            xi1 = np.array([-1,1]);
+            xi2 = np.array([-1,-1]);
+            N11=N1.subs(c1,xi1[j]).subs(c2,xi2[j]);
+            N12=N2.subs(c1,xi1[j]).subs(c2,xi2[j]);
+            if elemtyp=="Std":
+               Nstd=np.array([[N11,0,N12,0],[0,N11,0,N12]]);
+               return Nstd
+
+            elif elemtyp=="Enr" :
+               phi=phivalues[i];
+               Nenr=np.array([[N11,0,(N11*phi[0]),0,N12,0,(N12*phi[1]),0],\
+                              [0,N11,0,(N11*phi[0]),0,N12,0,(N12*phi[1])]]);
+               return Nenr
+           #Nstd=np.array([[N12,0,N13,0],[0,N12,0,N13]]);
+         #   for j in range(2):
+       # N11=N1.subs(c1,xi1[j]).subs(c2,xi2[j]);
+        #N12=N2.subs(c1,xi1[j]).subs(c2,xi2[j]);
+       # N13=N3.subs(c1,xi1[j]).subs(c2,xi2[j]);
+       # N14=N4.subs(c1,xi1[j]).subs(c2,xi2[j]);
+     
+       
+       
+       
+       # if elemtyp=="Std":
+         #  Nstd=np.array([[N11,0,N12,0,N13,0,N14,0],[0,N11,0,N12,0,N13,0,N14]]);
+        #   return Nstd
+
+       # elif elemtyp=="Enr" :
+      #     phi=phivalues[i];
+        #   Nenr=np.array([[N11,0,(N11*phi[0]),0,N12,0,(N12*phi[1]),0,N13,0,(N13*phi[2]),0,N14,0,(N14*phi[3]),0],\
+         #                  [0,N11,0,(N11*phi[0]),0,N12,0,(N12*phi[1]),0,N13,0,(N13*phi[2]),0,N14,0,\
+        #                                  (N14*phi[3])]]);
+        #   return Nenr             
    if i in elementnoc and i in belement:
            elemtyp="Std"
            #fbstd1=np.zeros(sizoff[1]);
-           for j in range(4):
-              N=shapefunctract(i, elemtyp, j)
-              dNdxi1=np.array([[(xi2[j]-1)/4,(1-xi2[j])/4,(1+xi2[j])/4,(-xi2[j]-1)/4],[(xi1[j]-1)/4,(-xi1[j]-1)/4,(1+xi1[j])/4,(1-xi1[j])/4]])  #Use this method(correct)
-              dNdxi1=np.round(dNdxi1,2)
-              kk=element[i]
-              Jac1=np.float32(np.matmul(dNdxi1,kk));           #jacobi matrix
-              det=np.linalg.det(Jac1);   
-              fb1=np.float32(wi[j]*np.matmul(np.transpose(N),tractforces)*t*det);
-              fbstd=fb1+fbstd;
+           for j in range(2):
+              N=shapefunctract(i, elemtyp, j,Edge)
+              #dNdxi1=np.array([[(xi2[j]-1)/4,(1-xi2[j])/4,(1+xi2[j])/4,(-xi2[j]-1)/4],[(xi1[j]-1)/4,(-xi1[j]-1)/4,(1+xi1[j])/4,(1-xi1[j])/4]])  #Use this method(correct)
+              #dNdxi1=np.round(dNdxi1,2)
+              #kk=element[i]
+             # Jac1=np.float32(np.matmul(dNdxi1,kk));           #jacobi matrix
+             #det=np.linalg.det(Jac1);   
+              #fb1=np.float32(wi[j]*np.matmul(np.transpose(N),tractforces)*t*det);
+              ft1=np.float32(wi[j]*np.matmul(np.transpose(N),tractforces)*t*(l/2));
+              ftstd1=ft1+ftstd1;
            if Edge == 'Edge1':
-               # if i !=cornerelements[0] and i !=cornerelements[1]:
-                   fbstd[0:2]=0;
-                   fbstd[6:8]=0;
-                   #fbstd[4:8]= fbstd[4:8]
+                   #ftstd[0:2]=0;
+                   #ftstd[6:8]=0;
+                   ftstd[2:6]=ftstd1
           
            elif Edge == 'Edge2':
-                 #if i!=cornerelements[1] and i!=cornerelements[3] :
-                     fbstd[0:4]=0;
+                     #ftstd[0:4]=0;
+                     ftstd[4:8]=ftstd1
                 
            elif Edge == 'Edge3':
-                   # if i!=cornerelements[2] and i!=cornerelements[3] :
-                        fbstd[2:6]=0;
-                    #    fbstd[0:2]=fbstd[0:2]/2
-                    #    fbstd[6:8]=fbstd[6:8]/2
-                  
+                        #ftstd[2:6]=0;
+                        ftstd[0:2]=ftstd1[0:2]
+                        ftstd[6:8]=ftstd1[2:4]
            elif Edge == 'Edge4':
-               #     if i!=cornerelements[0] and i!=cornerelements[2] :
-                        if i==0:
-                            fbstd[0:8]=0;
-                        else:    
-                            fbstd[4:8]=0;
-               #         fbstd[0:4]=fbstd[0:4]/2;
+                        ftstd[0:4]=ftstd1
+                        
+               
                
                     
            #fbstd1=fb1+fbstd1;
-           fb=fbstd.reshape(-1,1,order='F');
+           ft=ftstd.reshape(-1,1,order='F');
         
         
     
    elif i in elementc and i in belement :     ###Pending
            #fbenr1=np.zeros(sizoff1[1]);
            elemtyp="Enr"
-           for j in range(4):
+           for j in range(2):
                #elemtyp="Enr"
-               N=shapefunctract(i, elemtyp, j)
-               dNdxi1=np.array([[(xi2[j]-1)/4,(1-xi2[j])/4,(1+xi2[j])/4,(-xi2[j]-1)/4],[(xi1[j]-1)/4,(-xi1[j]-1)/4,(1+xi1[j])/4,(1-xi1[j])/4]])  #Use this method(correct)
-               dNdxi1=np.round(dNdxi1,2)
-               kk=element[i]
-               Jac1=np.float32(np.matmul(dNdxi1,kk));           #jacobi matrix
-               det=np.linalg.det(Jac1);   
-               fb1=np.float32(wi[j]*np.matmul(np.transpose(N),tractforces)*t*det);
-               fbenr=fb1+fbenr;
+               N=shapefunctract(i, elemtyp, j,Edge)
+               #dNdxi1=np.array([[(xi2[j]-1)/4,(1-xi2[j])/4,(1+xi2[j])/4,(-xi2[j]-1)/4],[(xi1[j]-1)/4,(-xi1[j]-1)/4,(1+xi1[j])/4,(1-xi1[j])/4]])  #Use this method(correct)
+              # dNdxi1=np.round(dNdxi1,2)
+              # kk=element[i]
+              # Jac1=np.float32(np.matmul(dNdxi1,kk));           #jacobi matrix
+              # det=np.linalg.det(Jac1);   
+              # fb1=np.float32(wi[j]*np.matmul(np.transpose(N),tractforces)*t*det);
+               ft1=np.float32(wi[j]*np.matmul(np.transpose(N),tractforces)*t*(l/2));
+               ftenr1=ft1+ftenr1;
            if Edge == 'Edge1':
-                     fbenr[4:12]=0;
+                     #ftenr[4:12]=0;
+                     ftenr[4:12]=ftenr1;
            elif Edge == 'Edge2':
-                     fbenr[0:8]=0;
+                     ftenr[8:16]=ftenr1;
            elif Edge == 'Edge3':
-                     fbenr[0:4]=0;
-                     fbenr[12:16]=0;
+                     ftenr[0:4]=ftenr1[0:4];
+                     ftenr[12:16]=ftenr1[4:8];
            else:
-                     fbenr[8:16]=0;
+                     ftenr[0:8]=ftenr1;
                      
                      
            #fbenr1=fb1+fbenr1;
-           fb=fbenr.reshape(-1,1,order='F');
+           ft=ftenr.reshape(-1,1,order='F');
                
    else:
         if i in elementnoc and i not in belement:
-            fb=fbstd.reshape(-1,1,order='F');
+            ft=ftstd.reshape(-1,1,order='F');
         elif i in elementc and i not in belement:
-            fb=fbenr.reshape(-1,1,order='F');
+            ft=ftenr.reshape(-1,1,order='F');
             
             
             
-   #totTractforce.append(fb);
-   return fb
+   F2.append(ft)
+ return F2
+
